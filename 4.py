53527aa08a5b10184b1fba3ca334c24ceeac0e6a
kirangunturu48 <105170858+kirangunturu48@users.noreply.github.com>
2022-11-21 17:02:08 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,19 +1,1 @@
 
-import requests
-from bs4 import BeautifulSoup
- 
-# url of the website
-doc = "https://www.geeksforgeeks.org"
- 
-# getting response object
-res = requests.get('https://www.geeksforgeeks.org', verify=False)
- 
-# Initialize the object with the document
-soup = BeautifulSoup(res.content, "html.parser")
- 
-# Get the whole body tag
-tag = soup.body
- 
-# Print each string recursively
-for string in tag.strings:
-    print(string)
